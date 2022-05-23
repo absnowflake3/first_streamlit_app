35de4b29976c4e64f244c7849278b4c50ec56283
absnowflake3 <105847265+absnowflake3@users.noreply.github.com>
2022-05-23 23:28:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,6 @@ streamlit.header('Fruityvice Fruit Advice!')
 fruit_choice = streamlit.text_input('What fruit would you like information about?', 'kiwi')
 streamlit.write('The user entered', fruit_choice)
 
-
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
@@ -38,13 +37,8 @@ streamlit.dataframe(fruityvice_normalized)
 import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-
 my_cur = my_cnx.cursor()
-
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-
 my_data_row = my_cur.fetchone()
-
 streamlit.text("Hello from Snowflake:")
-
 streamlit.text(my_data_row)
