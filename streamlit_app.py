463c47fb0f30bff1b4727356b0ee8e49e44be0ef
absnowflake3 <105847265+absnowflake3@users.noreply.github.com>
2022-06-02 17:35:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,8 @@ def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ this_fruit_choice)
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
-  streamlit.header('Fruityvice Fruit Advice!')
+
+streamlit.header('Fruityvice Fruit Advice!')
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
@@ -52,8 +53,8 @@ def get_fruit_load_list():
 if streamlit.button('Get Fruit load List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
-  streamlit.dataframe(my_data_rows)
   
+streamlit.dataframe(my_data_rows)
   
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
